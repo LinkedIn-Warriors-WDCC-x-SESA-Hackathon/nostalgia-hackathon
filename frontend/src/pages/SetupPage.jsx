0c11627dd8a82cc01504985cf351e6e28e5b3285
Dillon Mok <lightningxcookie@gmail.com>
2025-08-02 15:14:46 +1200

    Add search bar to setup page

--- frontend/src/pages/SetupPage.jsx
+++ frontend/src/pages/SetupPage.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import PageLayout from '../components/PageLayout';
-import Logo from '../assets/Logo.png';
-import BearLunchTrading from '../assets/BearLunchTrading.png';
+import React from "react";
+import PageLayout from "../components/PageLayout";
+import Logo from "../assets/Logo.png";
+import BearLunchTrading from "../assets/BearLunchTrading.png";
 
 const SetupPage = () => {
-  return (<>
-    <PageLayout title="Create your listing">
-      <div className="flex h-screen">
-        
-        <div className="w-100 bg-beige-darkest">
+    return (
+        <>
+            <PageLayout title="Create your listing">
+                <div className="flex h-screen">
+                    <div className="w-100 bg-beige-darkest">
+                        <div className="bg-beige-darker p-4">
+                            <div className="flex gap-4 mb-4">
+                                <img src={Logo} className="h-36"></img>
+                                <img
+                                    src={BearLunchTrading}
+                                    className="h-36"
+                                ></img>
+                            </div>
 
-          <div className="bg-beige-darker flex gap-4 p-4">
-            <img src={Logo} className="h-36"></img>
-            <img src={BearLunchTrading} className="h-36"></img>
-          </div>
+                            <input
+                                type="text"
+                                className="bg-white p-2 rounded-full w-full my-2"
+                                placeholder="Search..."
+                            ></input>
+                        </div>
 
-          <h1 className="text-teal-darker font-sans">setup page</h1>
-          <p>big lunchbox (item adding/setup panel thing) and display name form with submit button</p>
-        </div>
-      </div>
-    </PageLayout>
-  </>);
-}
+                        <h1 className="text-teal-darker font-sans">
+                            setup page
+                        </h1>
+                        <p>
+                            big lunchbox (item adding/setup panel thing) and
+                            display name form with submit button
+                        </p>
+                    </div>
+                </div>
+            </PageLayout>
+        </>
+    );
+};
 
-export default SetupPage
+export default SetupPage;
