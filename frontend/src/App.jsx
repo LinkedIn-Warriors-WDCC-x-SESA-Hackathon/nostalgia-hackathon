ad03aa5cb465434ce12655c18c6af3eabc133168
Abbey <abbeypmz@gmail.com>
2025-08-02 20:48:26 +1200

    added offers page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import SetupPage from './pages/SetupPage';
-import ListingsPage from './pages/ListingsPage';
-import NotFoundPage from './pages/NotFoundPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SetupPage from "./pages/SetupPage";
+import ListingsPage from "./pages/ListingsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import OffersPage from "./pages/OffersPage";
 
 function App() {
   return (
     <html>
-    <Routes>
-      <Route path="/" element={<SetupPage />} />
-      <Route path="/listings" element={<ListingsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<SetupPage />} />
+        <Route path="/listings" element={<ListingsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/offers" element={<OffersPage />} />
+      </Routes>
     </html>
   );
 }
 
-export default App
+export default App;
--- frontend/src/components/Lunchbox.jsx
+++ frontend/src/components/Lunchbox.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
+import React from "react";
 // Import food item assets
-import apple from '../assets/items/apple.svg';
-import carrot from '../assets/items/carrot.svg';
-import celery from '../assets/items/celery.svg';
-import cheese from '../assets/items/cheese.svg';
-import egg from '../assets/items/egg.svg';
-import hummus from '../assets/items/hummus.svg';
-import juice from '../assets/items/juice.svg';
-import potatoChips from '../assets/items/potato chips.svg';
+import apple from "/items/apple.svg";
+import carrot from "/items/carrot.svg";
+import celery from "/items/celery.svg";
+import cheese from "/items/cheese.svg";
+import egg from "/items/egg.svg";
+import hummus from "/items/hummus.svg";
+import juice from "/items/juice.svg";
+import potatoChips from "/items/potatochips.svg";
 
-const Lunchbox = ({ 
-  primaryColor = 'bg-purple-300', 
+const Lunchbox = ({
+  primaryColor = "bg-purple-300",
   tilt = 0,
-  size = 'medium',
-  className = '',
+  size = "medium",
+  className = "",
   items = [], // Array of items to display in the lunchbox
-  boxColor = 'bg-white bg-opacity-80' // Color of the internal compartment boxes
+  boxColor = "bg-white bg-opacity-80", // Color of the internal compartment boxes
 }) => {
   // Size configurations
   const sizeConfig = {
     small: {
-      container: 'w-[40rem] h-[20rem]',
-      padding: 'p-6'
+      container: "w-[40rem] h-[20rem]",
+      padding: "p-6",
     },
     medium: {
-      container: 'w-[64rem] h-[32rem]',
-      padding: 'p-8'
-    },  
+      container: "w-[64rem] h-[32rem]",
+      padding: "p-8",
+    },
     large: {
-      container: 'w-[80rem] h-[40rem]',
-      padding: 'p-10'
-    }
+      container: "w-[80rem] h-[40rem]",
+      padding: "p-10",
+    },
   };
 
   const config = sizeConfig[size];
-  
+
   // Map of available food items
   const foodAssets = {
     apple,
@@ -44,18 +44,18 @@ const Lunchbox = ({
     egg,
     hummus,
     juice,
-    potatoChips
+    potatoChips,
   };
 
   // Default items if none provided
-  const defaultItems = ['apple', 'cheese', 'carrot', 'juice'];
+  const defaultItems = ["apple", "cheese", "carrot", "juice"];
   const displayItems = items.length > 0 ? items : defaultItems;
-  
+
   // Generate transform style for tilt
   const transformStyle = tilt !== 0 ? { transform: `rotate(${tilt}deg)` } : {};
 
   return (
-    <div 
+    <div
       className={`${primaryColor} ${config.container} ${config.padding} rounded-2xl shadow-lg ${className}`}
       style={transformStyle}
     >
@@ -64,20 +64,24 @@ const Lunchbox = ({
         {/* Left side - two stacked boxes */}
         <div className="flex-1 flex flex-col gap-4">
           {/* Top left box */}
-          <div className={`flex-1 ${boxColor} rounded-xl flex items-center justify-center p-4 shadow-inner`}>
+          <div
+            className={`flex-1 ${boxColor} rounded-xl flex items-center justify-center p-4 shadow-inner`}
+          >
             {foodAssets[displayItems[0]] && (
-              <img 
-                src={foodAssets[displayItems[0]]} 
+              <img
+                src={foodAssets[displayItems[0]]}
                 alt={displayItems[0]}
                 className="w-16 h-16 object-contain"
               />
             )}
           </div>
           {/* Bottom left box */}
-          <div className={`flex-1 ${boxColor} rounded-xl flex items-center justify-center p-4 shadow-inner`}>
+          <div
+            className={`flex-1 ${boxColor} rounded-xl flex items-center justify-center p-4 shadow-inner`}
+          >
             {foodAssets[displayItems[1]] && (
-              <img 
-                src={foodAssets[displayItems[1]]} 
+              <img
+                src={foodAssets[displayItems[1]]}
                 alt={displayItems[1]}
                 className="w-16 h-16 object-contain"
               />
@@ -85,10 +89,12 @@ const Lunchbox = ({
           </div>
         </div>
         {/* Right side - one large box */}
-        <div className={`flex-1 ${boxColor} rounded-xl flex items-center justify-center p-4 shadow-inner`}>
+        <div
+          className={`flex-1 ${boxColor} rounded-xl flex items-center justify-center p-4 shadow-inner`}
+        >
           {foodAssets[displayItems[2]] && (
-            <img 
-              src={foodAssets[displayItems[2]]} 
+            <img
+              src={foodAssets[displayItems[2]]}
               alt={displayItems[2]}
               className="w-20 h-20 object-contain"
             />
