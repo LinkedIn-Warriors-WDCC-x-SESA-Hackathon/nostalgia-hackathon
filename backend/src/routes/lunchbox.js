1a8772e81691e02e772f365becb9dc5e544f1e3d
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-08-02 16:58:15 +1200

    create backend api routes for creating and retrieving lunchboxes

---
+++ backend/src/routes/lunchbox.js
@@ -0,0 +1,42 @@
+import express from 'express'
+
+import { lunchboxes } from '../store.js'
+
+const router = express.Router();
+
+// GET /lunchbox/:name
+// Get a lunchbox by name
+router.get('/:name', (req, res) => {
+  const lunchbox = lunchboxes.get(req.params.name)
+  if (!lunchbox) {
+    res.status(404).end()
+  } else {
+    res.status(200).send(lunchbox)
+  }
+});
+
+// GET /lunchbox
+// Get all lunchboxes listed in the marketplace
+router.get('', (req, res) => {
+  const lunchboxesFormatted = []
+  for (const [key, value] of lunchboxes) {
+    lunchboxesFormatted.push({
+      name: key,
+      lunchbox: value
+    })
+  }
+  res.status(200).send(lunchboxesFormatted)
+});
+
+// POST /lunchbox
+// Create a new lunchbox
+router.post('/', (req, res) => {
+  const name = req.body.name
+  const lunchbox = req.body.lunchbox
+
+  lunchboxes.set(name, lunchbox)
+
+  res.status(200).end()
+})
+
+export default router
---
+++ backend/src/store.js
@@ -0,0 +1,3 @@
+// Our "database" lol
+export const lunchboxes = new Map()
+export const tradeRequests = new Map()
