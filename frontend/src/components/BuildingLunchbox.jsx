e6380c0d0c10910ef6b9f5eb9618ebbbce1e67e9
Dillon Mok <lightningxcookie@gmail.com>
2025-08-02 13:18:09 +1200

    Add skeleton components for lunchboxes

---
+++ frontend/src/components/BuildingLunchbox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Lunchbox for "build your lunch box" page
+const BuildingLunchbox = () => {
+  return (
+    <div>BuildingLunchbox</div>
+  )
+}
+
+export default BuildingLunchbox
