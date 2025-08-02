de6c1516ae02f45b2a5346581060397609d802c7
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-08-02 17:31:40 +1200

    Create UserContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,17 @@ import SetupPage from './pages/SetupPage';
 import ListingsPage from './pages/ListingsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+import { UserProvider } from './context/UserProvider'
+
 function App() {
   return (
-    <html>
-    <Routes>
-      <Route path="/" element={<SetupPage />} />
-      <Route path="/listings" element={<ListingsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-    </html>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<SetupPage />} />
+        <Route path="/listings" element={<ListingsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
