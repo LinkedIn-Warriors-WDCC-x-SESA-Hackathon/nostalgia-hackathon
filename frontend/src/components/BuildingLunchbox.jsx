7442255f064639c0cbaf09b5ae4b0885d6bf259f
Dillon Mok <lightningxcookie@gmail.com>
2025-08-02 15:28:30 +1200

    Create placeholder building lunchbox

--- frontend/src/components/BuildingLunchbox.jsx
+++ frontend/src/components/BuildingLunchbox.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
 // Lunchbox for "build your lunch box" page
 const BuildingLunchbox = () => {
-  return (
-    <div>BuildingLunchbox</div>
-  )
-}
+    return (
+        <div className="bg-teal-darker w-150 h-100 rounded-2xl flex p-4 m-4">
+            <div className="w-1/3 pr-4 flex flex-col gap-4">
+                <div className="bg-teal h-1/2 rounded-2xl"></div>
+                <div className="bg-teal h-1/2 rounded-2xl"></div>
+            </div>
+            <div className="bg-teal w-2/3 rounded-2xl"></div>
+        </div>
+    );
+};
 
-export default BuildingLunchbox
+export default BuildingLunchbox;
